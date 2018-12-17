452776a5171ce20ab92595028f05bf9e81806639
Will Figueiredo <will@wfigueiredo.com>
2018-12-17 23:21:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
 class Artist
-  
+
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
   include Memorable::InstanceMethods
-  
+
   attr_accessor :name
   attr_reader :songs
 
